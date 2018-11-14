73db58b122b37332b2e5789a3d4b78db21b31dff
Birat Rai <biratrai@hotmail.com>
2018-11-14 17:25:14 -0500

    adding function to get list of apps

--- app/src/main/java/com/inspirenepal/customlauncher/MainActivity.kt
+++ app/src/main/java/com/inspirenepal/customlauncher/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.inspirenepal.customlauncher
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var apps: MutableList<AppInfo> = mutableListOf()
+    private var adapter: MyArrayAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadApps()
+        loadListView()
+//        addGridListeners()
     }
+
+    private fun addGridListeners() {
+
+    }
+
+    private fun loadListView() {
+        adapter = MyArrayAdapter(this, R.layout.grid_items, apps, layoutInflater)
+        gridAllApps.adapter = adapter
+    }
+
+    private fun loadApps() {
+        val i = Intent(Intent.ACTION_MAIN, null)
+        i.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        val availableApps = packageManager.queryIntentActivities(i, 0)
+        for (resolveInfo in availableApps) {
+            val appInfo = AppInfo(
+                resolveInfo.loadLabel(packageManager)
+                , resolveInfo.activityInfo.packageName
+                , resolveInfo.activityInfo.loadIcon(packageManager)
+            )
+            apps.add(appInfo)
+        }
+
+    }
+
 }
